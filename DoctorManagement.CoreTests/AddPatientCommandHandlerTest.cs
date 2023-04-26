2fc68a66c9eb51da0b6ba30ddf6bf166e207692f
rbocibo <rbocibo@jhb.dvt.co.za>
2023-04-26 11:38:43 +0200

    Service unit test and Handler unit test for adding patient

---
+++ DoctorManagement.CoreTests/AddPatientCommandHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿using DoctorManagement.Core.CQRS.Commands.AddPatientCommands;
+using DoctorManagement.Models.DTOs;
+using DoctorManagement.Services.Interfaces;
+using Moq;
+using System.Reflection.Metadata;
+using Xunit;
+
+namespace DoctorManagement.CoreTests
+{
+    public class AddPatientCommandHandlerTest
+    {
+        private readonly Mock<IPatientService> _patientService;
+
+        public AddPatientCommandHandlerTest()
+        {
+            _patientService = new Mock<IPatientService>();
+        }
+
+        [Fact]
+        public async Task AddPatientCommandHandler_Returns_Success()
+        {
+            var patientDTO = new PatientDTO()
+            {
+                PatientId = 1,
+                FirstName = "name",
+                LastName = "surname",
+                PhoneNumber = "0123456789",
+                Email = "email@gmail.com",
+            };
+
+            _patientService.Setup(x => x.AddPatient(It.IsAny<PatientDTO>()))
+                .Returns(Task.FromResult(patientDTO));
+
+            var handler = new AddPatientCommandHandler(_patientService.Object);
+
+            //Act
+            var handlerResponse = handler.Handle(new AddPatientCommand(patientDTO), CancellationToken.None);
+            var result = handlerResponse.Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, patientDTO);
+            
+        }
+    }
+}
---
+++ DoctorManagement.ServiceTests/Services/AddPatientServiceTest.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using DoctorManagement.Domain.Entities;
+using DoctorManagement.Domain.Interfaces;
+using DoctorManagement.Domain.UnitOfWorkInterface;
+using DoctorManagement.Models.DTOs;
+using DoctorManagement.Services.Services;
+using Moq;
+using Xunit;
+
+namespace DoctorManagement.ServiceTests
+{
+    public class AddPatientServiceTest
+    {
+        private readonly Mock<IPatientRepository> _patientRepository;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public AddPatientServiceTest()
+        {
+            _patientRepository = new Mock<IPatientRepository>();
+            _mapper = new Mock<IMapper>();
+            _unitOfWork = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task AddPatient_Returns_Success()
+        {
+
+
+            //Arrange
+            Patient patient = new Patient()
+            {
+                PatientId = 1,
+                FirstName = "name",
+                LastName = "surname",
+                PhoneNumber = "123456789",
+                Email = "email@gmail.com",
+            };
+
+            PatientDTO patientDTO = new PatientDTO()
+            {
+                PatientId = 1,
+                FirstName = "name",
+                LastName = "surname",
+                PhoneNumber = "123456789",
+                Email = "email@gmail.com"
+            };
+
+            _patientRepository.Setup(x => x.CreateAsync(It.IsAny<Patient>()))
+                .Returns(Task.FromResult(patient));
+            _mapper.Setup(x => x.Map<PatientDTO>(It.Is<Patient>(m => m.Equals(patient))))
+                .Returns(patientDTO);
+
+            var patientService = new PatientService(_patientRepository.Object, _unitOfWork.Object, _mapper.Object);
+
+            //Act
+            var serviceResponse = patientService.AddPatient(patientDTO);
+
+            //Assert
+            Assert.NotNull(serviceResponse);
+            Assert.Equal(patient.FirstName, patientDTO.FirstName);
+            Assert.Equal(patient.LastName, patientDTO.LastName);
+            Assert.Equal(patient.Email, patientDTO.Email);
+            Assert.Equal(patient.PatientId, patientDTO.PatientId);
+        }
+    }
+}
